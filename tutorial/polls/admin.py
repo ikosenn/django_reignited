066a7dacf20ed3dd123790dc78e99317856ea731
Ian Kosen <ian.kosen@savannahinformatics.com>
2015-05-20 16:13:44 +0300

    Put Question Admin fields in a fieldset and added a collapse class to the date field

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -4,8 +4,13 @@ from django.contrib import admin
 from .models import Question
 
 
-
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+
+    #fields = ['pub_date', 'question_text']
+
+    fieldsets = [
+        (None, {'fields' : ['question_text']}),
+        ('Date Information', { 'fields' : ['pub_date'], 'classes': ['collapse']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
