eb531d40d5b1381def8f08225f05f45be1410f30
Ian Kosen <ian.kosen@savannahinformatics.com>
2015-05-20 11:12:37 +0300

    Created a polls app

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_reignited',
+        'USER': 'django_reignited',
+        'PASSWORD': 'django_reignited',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
